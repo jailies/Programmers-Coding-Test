2766aa72a83e3a882ef2eacf8f214e6d3ed59a36
jailies <jailies2004@gmail.com>
2023-01-10 17:08:55 +0900

    lv 1

---
+++ 완주하지 못한 선수.py
@@ -0,0 +1,7 @@
+def solution(participant, completion):
+    answer =''
+    setp = set(participant)
+    
+    return answer
+
+print(solution(["mislav", "stanko", "mislav", "ana"],["stanko", "ana", "mislav"]))
